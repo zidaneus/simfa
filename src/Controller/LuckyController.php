3223752ebfbcb06667327b3ab817801292f8feb7
Zidane <sazonov@clientbase.ru>
2018-02-23 19:12:07 +0300

    2 learn routing

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -26,4 +26,36 @@ class LuckyController extends Controller
             'number' => $number,
         ]);
     }
+
+    /**
+     * @Route("/lucky/{page}", name="lucky_list", requirements={"page"="\d+"})
+     */
+    public function list($page = 1)
+    {
+        return new Response($page);
+    }
+
+    /**
+     * @Route("/lucky/{slug}", name="lucky_show")
+     */
+    public function show($slug)
+    {
+        return new Response('slug:' . $slug);
+    }
+
+    /**
+     * @Route(
+     *     "/lucky/{_locale}/{year}/{slug}.{_format}",
+     *     defaults={"_format": "html"},
+     *     requirements={
+     *         "_locale": "en|ru",
+     *         "_format": "html|rss",
+     *         "year": "\d+"
+     *     }
+     * )
+     */
+    public function locale($_locale, $year, $slug)
+    {
+        return new Response('locale:' . $_locale . ' year:' . $year . ' slug:' . $slug);
+    }
 }
